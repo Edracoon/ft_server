7d7deddffbef13001a533797f16c3cbddb9d54cc
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-03-05 15:06:51 +0100

    commit

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,25 @@ FROM debian:buster
 
 RUN apt update -y
 RUN apt-get -y install wget curl
-RUN apt-get -y install vim \
-						nginx \
-						openssl
+RUN apt-get -y install nginx \
+						openssl \
+						php7.3 \
+						php7.3-fpm \
+						php7.3-mysql \
+						php-common \
+						php7.3-cli \
+						php7.3-common \
+						php7.3-json \
+						php7.3-opcache \
+						php7.3-readline \
+						mariadb-server \
+						mariadb-client
 
 COPY srcs/launch.sh ./
+COPY srcs/default ./
+COPY srcs/index.php ./
 
 # NGINX CONFIG
-CMD bash /launch.sh
+CMD bash launch.sh
 
 EXPOSE 80
--- srcs/launch.sh
+++ srcs/launch.sh
@@ -1,6 +1,15 @@
-# NginX CONFIG
-sudo ufw allow 'Nginx HTTP'
-service nginx start && sleep infinity
+# NginX
+rm /etc/nginx/sites-available/default
+mv /default /etc/nginx/sites-available
 
-#OpenSSL CONFIG
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=emma/CN=localhost"
+#SSL
+mkdir etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Nice/L=Nice/O=42 School/OU=Edgar/CN=localhost"
+
+#PHP
+
+#Launch
+service nginx start
+service php7.3-fpm start
+service mysql start
+sleep infinity
